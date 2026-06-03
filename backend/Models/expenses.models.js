cbfe5211924ad359d0a451bb3acc62be31e1c1af
Bolice1 <komezusengebolice@gmail.com>
2026-06-03 14:56:59 +0200

    clear: removed unnecessary files

--- backend/Models/expenses.models.js
+++ backend/Models/expenses.models.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const expenseSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true },
-    description: { type: String, default: '', trim: true },
+    description: { type: Scleartring, default: '', trim: true },
     amount: { type: Number, required: true, min: 0 },
     category: {
         type: String,
